43573c791036a07e68e55d403132298885396afe
WyethBlumberg <wyethblumberg@gmail.com>
2024-01-23 17:14:31 -0500

    Practical

---
+++ Practical_2.R
@@ -0,0 +1,14 @@
+library(usethis)
+library(gitcreds)
+
+git_sitrep()
+use_git_config(
+  user.name = "WyethBlumberg",
+  user.email = "wyethblumberg@gmail.com"
+)
+
+create_github_token()
+
+gitcreds_set()
+
+
